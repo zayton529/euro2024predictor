1b4d82efa497ae8b48d8324f40f29e08755c3b3b
zayton529 <zayton.honeycutt5@gmail.com>
2024-06-04 08:02:16 +0200

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+module.exports = function(passport) {
+    passport.use(new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username });
+            if (!user) {
+                return done(null, false, { message: 'No user found' });
+            }
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Incorrect password' });
+            }
+        } catch (err) {
+            return done(err);
+        }
+    }));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
+            done(null, user);
+        } catch (err) {
+            done(err);
+        }
+    });
+};
